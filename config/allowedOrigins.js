ebd269a18200cb4f258347cca6787073f0184f6e
maja-98 <majajouhar@gmail.com>
2023-02-19 19:28:38 +0530

    updated allowed orgins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://dubai-pardha-palace.netlify.app/',
-    'http://dubai-pardha-palace.netlify.app/'
+    'https://dubai-pardha-palace.netlify.app',
+    'http://dubai-pardha-palace.netlify.app'
 ]
 module.exports = allowedOrigins
