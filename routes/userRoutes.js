ad7d6ab17becafa1043c70edf6020b1d5914b6de
maja-98 <majajouhar@gmail.com>
2023-03-16 23:29:37 +0530

    user permission changed

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,10 +3,11 @@ const router = express.Router()
 const userController = require('../controllers/userControllers')
 const verifyJWT = require('../middlewares/verifyJWT')
 
+router.route('/').post(userController.createNewUser)
 
+router.use(verifyJWT)
 router.route('/')
-    .get(userController.getAllUsers)
-    .post(userController.createNewUser)
+    .get(userController.getAllUsers)   
     .patch(userController.updateUser)
     .delete(userController.deleteUser)
 router.route('/:userId')
