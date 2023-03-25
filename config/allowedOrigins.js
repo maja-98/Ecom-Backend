9e62e0548940f6f1f838965efae70a251343dc4a
maja-98 <majajouhar@gmail.com>
2023-03-25 21:01:25 +0530

    cors updated

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,5 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://dubai-pardha-palace.netlify.app',
-    'http://dubai-pardha-palace.netlify.app'
+    'https://mens-boutique.netlify.app/'
 ]
 module.exports = allowedOrigins
